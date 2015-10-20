a9a4e3b3ad8d5cd49a42a98093c222ec27c7c346
Dylan Tan <colin4939@gmail.com>
2015-10-20 03:07:10 +0000

    slightly motification

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -4,8 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-
 var mongoose = require('mongoose');
 require('./models/Posts');
 require('./models/Comments');
@@ -15,9 +13,8 @@ var users = require('./routes/users');
 
 var app = express();
 
-
+//connect to local db 
 mongoose.connect('mongodb://localhost/news');
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
